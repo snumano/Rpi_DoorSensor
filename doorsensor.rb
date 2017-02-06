ab653d1e05f31f1ce7dac58cd46f993e9ad2c2bf
Shugo Numano <snumano@gmail.com>
2017-02-06 12:06:26 +0000

    revise code

--- doorsensor.rb
+++ doorsensor.rb
@@ -10,17 +10,19 @@ require "faraday"
 GPIO26 = 25
 
 # init
-bridge = 1
-id = 1
+bridge = 3
+id = 3
 
 # for API
 bridge_ip = ["192.168.11.3", "192.168.11.2", "192.168.11.4"]
 api_key   = ["0XKpToZs32abteyjeo7VGED-8kilfZ5xASozaZ9-", "FHUpTcDacBmSNXrHCTSwSKPJ2DY2tV22cWcLuelY", "QaiyESlyKVQjjpsCikFie6lZw41d0RLOMEiHpTOi"]
 
 def api_put(bridge_ip, api_key, id, key, value)
-  res = Faraday.put "http://" + bridge_ip + "/api/" + api_key + "/lights/" + id + "/state", "{\"hue\": #{value}}"
+  p id
+  p value
+  res = Faraday.put "http://" + bridge_ip + "/api/" + api_key + "/lights/" + id.to_s + "/state", "{\"hue\": #{value.to_s}}"
 
-  p "http://" + bridge_ip + "/api/" + api_key + "/lights/" + id + "/state"
+  p "http://" + bridge_ip + "/api/" + api_key + "/lights/" + id.to_s + "/state"
   p key
   p value
   p '==='
@@ -37,10 +39,10 @@ loop do
 
   if state == 0
     api_put(bridge_ip[bridge - 1], api_key[bridge - 1], id, "hue", 25500) # Green
-  elsif stet == 1
-    api_put(bridge_ip[bridge - 1], api_key[bridge - 1], id, "hue", 46920) # Blue
-  else
+  elsif state == 1
     api_put(bridge_ip[bridge - 1], api_key[bridge - 1], id, "hue", 65280) # Red
+  else
+    api_put(bridge_ip[bridge - 1], api_key[bridge - 1], id, "hue", 46920) # Blue
   end
   
   sleep(1)
